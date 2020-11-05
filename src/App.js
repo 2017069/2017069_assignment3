0da93b81c63fc327cde7c4897767dba5f44708b6
2017069 <73095187+2017069@users.noreply.github.com>
2020-11-05 22:18:12 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
       //    Edit <code>src/App.js</code> and save to reload.
         </p>
         
-          Ammar ahmed 
+          Ammar ahmed 20101010
      
       </header>
     </div>
